d28e5e7b1a2b7eef184755d04fef3a4c99db1676
DanteMaciasCaracosa <l22120685@morelia.tecnm.mx>
2025-05-19 12:17:44 -0600

    Footer con los apartados

--- react/src/components/Footer.jsx
+++ react/src/components/Footer.jsx
@@ -1,3 +1,4 @@
+//Front del footer editado con los apartados
 import React from 'react';
 
 const FooterComponent = () => {
@@ -7,29 +8,46 @@ const FooterComponent = () => {
                 <div className="sb_footer-links grid grid-cols-1 sm:grid-cols-3 gap-6 justify-items-center">
                     {/* Contactanos */}
                     <div className="sb_footer-links_div mb-4">
+
                         <h4 className="text-lg font-bold">Contáctanos</h4>
+
                         <a href="/trabajar"><p>Trabajar con nosotros</p></a>
+
                         <a href="/nosotros"><p>Acerca de nosotros</p></a>
+
                         <a href="/corporativo"><p>Información corporativa</p></a>
+
                         <a href="/prensa"><p>Departamento de prensa</p></a>
+
                         <a href="/science"><p>Pony Science</p></a>
                     </div>
 
                     {/* Método de Pago */}
                     <div className="sb_footer-links_div mb-4">
                         <h4 className="text-lg font-bold">Método de Pago</h4>
+
                         <a href="/debitocredito"><p>Tarjetas de crédito y débito</p></a>
+
                         <a href="/tarjetaregalo"><p>Tarjetas de regalo</p></a>
+
                         <a href="/efectivo"><p>Pago en efectivo</p></a>
+
                         <a href="/MSI"><p>Pago a meses</p></a>
                     </div>
 
                     {/* Ayuda */}
                     <div className="sb_footer-links_div mb-4">
+
                         <h4 className="text-lg font-bold">Ayuda</h4>
+
+
                         <a href="/devoluciones"><p>Devolución</p></a>
+
+
                         <a href="/dispositivoscontenido"><p>Gestionar dispositivos o contenido</p></a>
+
                         <a href="/listaderegalos"><p>Listas de regalos</p></a>
+                        
                         <a href="/ayuda"><p>Ayuda</p></a>
                     </div>
                 </div>
