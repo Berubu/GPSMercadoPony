a2649cbc462f24b633b9f8a84c48f65518da88b8
DanteMaciasCaracosa <l22120685@morelia.tecnm.mx>
2025-05-19 12:34:25 -0600

    Vista de compra con footer y header

--- react/src/components/VistaCompra.test.jsx
+++ react/src/components/VistaCompra.test.jsx
@@ -1,11 +1,17 @@
+// Compra con footer y header
 import React from 'react';
+
 import { render, screen, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
+
 import VistaCompra from './VistaCompra';
+
 
 // Mocks para componentes de encabezado y pie de página
 jest.mock('./Header', () => () => <div>Mock Header</div>);
+
 jest.mock('./Footer', () => () => <div>Mock Footer</div>);
+
 
 describe('VistaCompra Component', () => {
   beforeEach(() => {
@@ -15,6 +21,7 @@ describe('VistaCompra Component', () => {
         json: () => Promise.resolve({ nombre: 'Producto de prueba', precio: 100 }),
       })
     );
+
   });
 
   afterEach(() => {
@@ -26,22 +33,33 @@ describe('VistaCompra Component', () => {
     expect(screen.getByText(/Cargando.../i)).toBeInTheDocument();
   });
 
-  test('muestra el mensaje "No se encontró el producto" si no hay datos', async () => {
+
+  test('muestra el mensaje "No se encontró el producto" si no hay datos', async ()
+   => {
     global.fetch = jest.fn(() => Promise.resolve({ json: () => Promise.resolve(null) }));
     render(<VistaCompra />);
+
 
     await waitFor(() => expect(screen.getByText(/No se encontró el producto/i)).toBeInTheDocument());
   });
 
   test('muestra los datos del producto después de cargar', async () => {
+
     render(<VistaCompra />);
 
     await waitFor(() => {
       expect(screen.getByText(/Enviar a Domicilio/i)).toBeInTheDocument();
       expect(screen.getByText(/Dirección Generica/i)).toBeInTheDocument();
+
+
       expect(screen.getByText(/Forma de Pago/i)).toBeInTheDocument();
       expect(screen.getByText(/Producto: \$100/i)).toBeInTheDocument();
+
+
       expect(screen.getByText(/Total: \$150/i)).toBeInTheDocument();
+
+
     });
   });
 });
+
