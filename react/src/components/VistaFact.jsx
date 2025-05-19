4ff33a020f405413b93a6755d55cc555a6abf94f
DanteMaciasCaracosa <l22120685@morelia.tecnm.mx>
2025-05-19 12:35:25 -0600

    Vista de factura actualizada

--- react/src/components/VistaFact.jsx
+++ react/src/components/VistaFact.jsx
@@ -1,9 +1,14 @@
+//Vista de factura final
 import React, { useState } from 'react';
 import HeaderComponent from './Header';
+
 import FooterComponent from './Footer';
+
+
 
 function VistaFact() {
   const [urlFactura, setUrlFactura] = useState(null);
+
 
   const descargarFactura = async () => {
     try {
@@ -13,6 +18,7 @@ function VistaFact() {
     } catch (error) {
       console.error('Error al descargar la factura:', error);
     }
+
   };
 
   return (
@@ -20,10 +26,13 @@ function VistaFact() {
       <HeaderComponent />
       <div className="flex flex-col items-center">
         <section className="bg-gray-300 w-full py-10 text-center">
+
         <h2 className="text-2xl">Gracias por tu compra</h2>
         <h3 className="text-lg mt-2">Tu pedido llegara dentro de unos días</h3>
         <img
         src="/public/img/iconfact.png"
+
+
         alt="papa"
         className="mx-auto w-[150px] mt-4"
         />
@@ -31,14 +40,17 @@ function VistaFact() {
             <p className="text-lg mb-2">Puede descargar su factura aquí</p>
             <div className="flex space-x-4">
               <button
+
                   onClick={() => window.location.href = 'http://127.0.0.1:8000/compras/2/invoice'}
                   className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded"
+
               >
                 Descargar factura
               </button>
 
              <button
                   onClick={() => window.location.href = 'http://127.0.0.1:8000/compras'}
+
                   className="bg-green-500 hover:bg-green-600 text-white font-semibold py-2 px-4 rounded"
               >
                 Explorar más
